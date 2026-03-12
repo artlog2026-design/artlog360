c9341aec0d2775eae203a3371937dda34028a6cd
mac <mac@macui-MacBookAir.local>
2026-03-12 14:19:01 +0900

    fix build

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../artlog360-mvp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
